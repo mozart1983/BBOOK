540ed25802fd7a4dfc8c94b625c904f453971d6d
mozart2014 <diaotianping@163.com>
2015-12-15 16:03:33 +0800

    增加工具类

---
+++ DBOOKTOP/src/com/douban/tools/CJsonParser.java
@@ -0,0 +1,17 @@
+package com.douban.tools;
+
+public class CJsonParser {
+
+	public CJsonParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
